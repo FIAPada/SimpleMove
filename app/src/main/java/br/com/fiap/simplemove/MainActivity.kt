fb126e4a0b926ea9cd101eba273b26f6e2cc5856
Rodrigo Moreira Ribeiro <mulukou@gmail.com>
2024-08-06 17:53:53 -0300

    Added Firebase login

--- app/src/main/java/br/com/fiap/simplemove/MainActivity.kt
+++ app/src/main/java/br/com/fiap/simplemove/MainActivity.kt
@@ -1,17 +1,31 @@
 package br.com.fiap.simplemove
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import java.io.IOException
+import kotlin.concurrent.thread
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
+    private var databaseUrl = "https://simple-move-a8a86-default-rtdb.firebaseio.com"
+
+    private lateinit var user: EditText
+    private lateinit var pass: EditText
+
+    private val clientHttp = OkHttpClient.Builder().build()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,42 +36,76 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        user = findViewById<EditText>(R.id.edt_login)
+        pass = findViewById<EditText>(R.id.edt_senha)
+    }
 
-        val usuario = findViewById<EditText>(R.id.edt_login)
-        val senha = findViewById<EditText>(R.id.edt_senha)
-        val registrar=findViewById<Button>(R.id.btn_registrar)
-        val login=findViewById<Button>(R.id.btn_login)
+    fun onClickRegister(view: View) {
+        val userText = user.editableText.toString()
+        val passText = pass.editableText.toString()
 
+        thread {
+            val usuarioJson = """
+            {
+                "user": "$userText",
+                "pass": "$passText"
+            }
+        """.trimIndent()
 
-        registrar.setOnClickListener {
-            val credenciais=this.getSharedPreferences("credenciais", Context.MODE_PRIVATE)
-            val editor =credenciais.edit()
+            val body = RequestBody.create(MediaType.parse("application/json"), usuarioJson)
 
-            editor.putString("User", usuario.editableText.toString())
-            editor.putString("Password", senha.editableText.toString())
-            editor.apply()
+            val request = Request.Builder().url("$databaseUrl/users.json").post(body).build()
 
-            Toast.makeText(this, "Usuário registrado com sucesso!", Toast.LENGTH_SHORT).show()
-        }
+            val call = clientHttp.newCall(request)
 
-        login.setOnClickListener {
-            val i= Intent(this , HomePage::class.java)
-
-            val credenciaisExistentes=this.getSharedPreferences("credenciais", Context.MODE_PRIVATE)
-
-            val user= credenciaisExistentes.getString("User","None")
-            val password= credenciaisExistentes.getString("Password","")
-
-            if (user.equals("None") or (usuario.editableText.toString() == "")){
-                Toast.makeText(this, "Registre-se preenchendo suas informações e clicando em registrar", Toast.LENGTH_LONG).show()
-            }else if(user.equals(usuario.editableText.toString()) and password.equals(senha.editableText.toString())){
-                startActivity(i)
-            }else{
-                Toast.makeText(this, "Usuário ou senha incorretos.", Toast.LENGTH_SHORT).show()
+            var response: Response? = null;
+            try {
+                response = call.execute()
+                Log.i("RESPONSE REGISTER", response.body().toString())
+            } catch (error: IOException) {
+                error.printStackTrace()
             }
         }
 
+        Toast.makeText(this, "Usuário registrado com sucesso!", Toast.LENGTH_SHORT).show()
+    }
 
+    fun onClickLogin(view: View) {
+        thread {
+            val request = Request.Builder().url("$databaseUrl/users.json").get().build()
+            val call = clientHttp.newCall(request)
 
+            var response: Response? = null;
+            try {
+                response = call.execute()
+                val responseJson = response.body()!!.string()
+
+                Log.i("RESPONSE LOGIN", responseJson)
+
+                val gson = Gson().fromJson(responseJson, Map::class.java)
+                val users = gson.values
+
+                for (user in users) {
+                    val userJson = user as Map<*, *>
+                    val userText = userJson["user"] as String
+                    val passText = userJson["pass"] as String
+
+                    if (userText == this.user.editableText.toString() && passText == this.pass.editableText.toString()) {
+                        runOnUiThread {
+                            Toast.makeText(this, "Login efetuado com sucesso!", Toast.LENGTH_SHORT)
+                                .show()
+
+                            val i= Intent(this , HomePage::class.java)
+
+                            startActivity(i)
+                        }
+                        return@thread
+                    }
+                }
+            } catch (error: IOException) {
+                Toast.makeText(this, "Usuário não existente ou senha incorreta.", Toast.LENGTH_SHORT).show()
+                error.printStackTrace()
+            }
+        }
     }
 }
