7d5b00074c681427a4e35d47485e1eafbebc527c
Pedro Leta <pedrogleta@gmail.com>
2024-08-06 22:18:00 -0300

    add google maps integration

---
+++ app/src/main/java/br/com/fiap/simplemove/MapActivity.kt
@@ -0,0 +1,31 @@
+package br.com.fiap.simplemove
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapActivity: AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.maps_layout)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(mapa: GoogleMap) {
+        mMap = mapa
+
+        val paulista = LatLng(-23.564055206524376, -46.65240007651715)
+        mMap.addMarker(MarkerOptions().position(paulista).title("FIAP Paulista"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(paulista))
+    }
+}
